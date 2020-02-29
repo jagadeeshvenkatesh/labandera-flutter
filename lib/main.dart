40fb209f1731b75139d800bd149a221901efce72
John Cosio <mail@jcos.io>
2020-02-29 23:23:08 +0800

    Almost working. Only need to add condition to check when date is empty then skip

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,15 @@ import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:date_format/date_format.dart';
 import 'package:http/http.dart' as http;
+
+String convertDateFromString(String strDate){
+  DateTime todayDate = DateTime.parse(strDate);
+  print(todayDate);
+  print(formatDate(todayDate, [MM, ' ', dd, ', ', yyyy]));
+  return formatDate(todayDate, [MM, ' ', dd, ', ', yyyy]);
+}
 
 Future<List<Customer>> fetchCustomers(http.Client client) async {
   final response =
@@ -24,13 +32,21 @@ class Customer {
   final int id;
   final String name;
   final String status;
-  Customer({this.id, this.name, this.status});
+  final String price;
+  final String isPaid;
+  final String dateReceived;
+  final String dateReturned;
+  Customer({this.id, this.name, this.status, this.price, this.isPaid, this.dateReceived, this.dateReturned});
 
   factory Customer.fromJson(Map<String, dynamic> json) {
     return Customer(
       id: json['id'] as int,
       name: json['name'] as String,
       status: json['status'] as String,
+      price: json['price'] as String,
+      isPaid: json['isPaid'] as String,
+      dateReceived: json['dateReceived'] as String,
+      dateReturned: json['dateReturned'] as String,
     );
   }
 }
@@ -122,11 +138,203 @@ class CustomerScreen extends StatelessWidget {
     // Use the Customer to create the UI.
     return Scaffold(
       appBar: AppBar(
-        title: Text(customer.name),
+        title: Text('Labada Info'),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Text(customer.status),
+      body: ListView(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: Row(
+              children: [
+                Expanded(
+                  /*1*/
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      /*2*/
+                      Container(
+                        padding: const EdgeInsets.only(bottom: 12),
+                        child: Text(
+                          customer.name,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        'Customer',
+                        style: TextStyle(
+                          color: Colors.grey[500],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: Row(
+              children: [
+                Expanded(
+                  /*1*/
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      /*2*/
+                      Container(
+                        padding: const EdgeInsets.only(bottom: 12),
+                        child: Text(
+                          customer.status,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        'Status',
+                        style: TextStyle(
+                          color: Colors.grey[500],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: Row(
+              children: [
+                Expanded(
+                  /*1*/
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      /*2*/
+                      Container(
+                        padding: const EdgeInsets.only(bottom: 12),
+                        child: Text(
+                          customer.price,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        'Price',
+                        style: TextStyle(
+                          color: Colors.grey[500],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: Row(
+              children: [
+                Expanded(
+                  /*1*/
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      /*2*/
+                      Container(
+                        padding: const EdgeInsets.only(bottom: 12),
+                        child: Text(
+                          customer.isPaid,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        'Payment status',
+                        style: TextStyle(
+                          color: Colors.grey[500],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: Row(
+              children: [
+                Expanded(
+                  /*1*/
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      /*2*/
+                      Container(
+                        padding: const EdgeInsets.only(bottom: 12),
+                        child: Text(
+                          convertDateFromString(customer.dateReceived),
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        'Date laundry received from customer',
+                        style: TextStyle(
+                          color: Colors.grey[500],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: Row(
+              children: [
+                Expanded(
+                  /*1*/
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      /*2*/
+                      Container(
+                        padding: const EdgeInsets.only(bottom: 12),
+                        child: Text(
+                          '',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        'Date laundry returned to customer',
+                        style: TextStyle(
+                          color: Colors.grey[500],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
