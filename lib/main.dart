7c94d662d5edd4fc0cbc1ddc41dd409e5a28ad86
John Cosio <mail@jcos.io>
2020-04-26 19:48:36 +0800

    Working login
    Followed the guide from dev.to
    
    https://dev.to/carminezacc/user-authentication-jwt-authorization-with-flutter-and-node-176l

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'dart:convert' show json, base64, ascii;
+
+const String apiBase = 'https://api.labada.tigasoft.dev/api';
+final storage = FlutterSecureStorage();
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  Future<String> get jwtOrEmpty async {
+    var jwt = await storage.read(key: "auth");
+    if(jwt == null) return "";
+    return jwt;
+  }
+
   @override
   Widget build(BuildContext context) {
-    final appTitle = 'Labandera Laundry App';
+    final appTitle = 'Labandera Laundry Shop';
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -14,30 +26,55 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         // This is the theme of your application.
         //
-        // Try running your application with "flutter run". You'll see the
+        // Try running your application with 'flutter run'. You'll see the
         // application has a blue toolbar. Then, without quitting the app, try
         // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // 'hot reload' (press 'r' in the console where you ran 'flutter run',
+        // or simply save your changes to 'hot reload' in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.teal,
       ),
-      home: MyHomePage(title: appTitle),
+      home: LoginPage(title: appTitle),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class LoginPage extends StatefulWidget {
   final String title;
 
-  MyHomePage({Key key, this.title}) : super(key: key);
+  LoginPage({Key key, this.title}) : super(key: key);
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  void displayDialog(context, title, text) => showDialog(
+      context: context,
+      builder: (context) =>
+        AlertDialog(
+          title: Text(title),
+          content: Text(text)
+        ),
+    );
+
+  Future<String> attemptLogIn(String email, String password) async {
+    print('$apiBase/auth/login');
+    var res = await http.post(
+      '$apiBase/auth/login',
+      body: {
+        'email': email,
+        'password': password
+      }
+    );
+    if(res.statusCode == 200) return res.body;
+    return null;
+  }
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _LoginPageState createState() => _LoginPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _LoginPageState extends State<LoginPage> {
   TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
 
   @override
@@ -52,9 +89,10 @@ class _MyHomePageState extends State<MyHomePage> {
     final emailField = TextField(
       obscureText: false,
       style: style,
+      controller: widget._emailController,
       decoration: InputDecoration(
           contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-          hintText: "Email",
+          hintText: 'Email',
           border:
           OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
     );
@@ -62,9 +100,10 @@ class _MyHomePageState extends State<MyHomePage> {
     final passwordField = TextField(
       obscureText: true,
       style: style,
+      controller: widget._passwordController,
       decoration: InputDecoration(
           contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-          hintText: "Password",
+          hintText: 'Password',
           border:
           OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
     );
@@ -72,12 +111,29 @@ class _MyHomePageState extends State<MyHomePage> {
     final loginButon = Material(
       elevation: 5.0,
       borderRadius: BorderRadius.circular(30.0),
-      color: Color(0xff01A0C7),
+      color: Colors.teal,
       child: MaterialButton(
         minWidth: MediaQuery.of(context).size.width,
         padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        onPressed: () {},
-        child: Text("Login",
+        onPressed: () async {
+                var email = widget._emailController.text;
+                var password = widget._passwordController.text;
+                var auth = {};
+                var jwt = await widget.attemptLogIn(email, password);
+                if(jwt != null) {
+                  auth = json.decode(jwt);
+                  storage.write(key: "jwt", value: auth['token']);
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => HomePage.fromBase64(jwt)
+                    )
+                  );
+                } else {
+                  widget.displayDialog(context, "An Error Occurred", "No account was found matching that email and password");
+                }
+              },
+        child: Text('Login',
             textAlign: TextAlign.center,
             style: style.copyWith(
                 color: Colors.white, fontWeight: FontWeight.bold)),
@@ -101,7 +157,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     SizedBox(
                       height: 155.0,
                       child: Image.asset(
-                        "assets/logo.jpg",
+                        'assets/logo.jpg',
                         fit: BoxFit.contain,
                       ),
                     ),
@@ -124,4 +180,36 @@ class _MyHomePageState extends State<MyHomePage> {
         )
     );
   }
+}
+
+class HomePage extends StatelessWidget {
+  HomePage(this.jwt, this.payload);
+  
+  factory HomePage.fromBase64(String jwt) =>
+    HomePage(
+      jwt,
+      json.decode(jwt)
+    );
+
+  final String jwt;
+  final Map<String, dynamic> payload;
+
+  @override
+  Widget build(BuildContext context) =>
+    Scaffold(
+      appBar: AppBar(title: Text(payload['token'])),
+      body: Center(
+        child: FutureBuilder(
+          future: http.read('$apiBase/orders', headers: {"x-access-token": payload['token']}),
+          builder: (context, snapshot) =>
+            snapshot.hasData ?
+            Column(children: <Widget>[
+              Text("${payload['auth']}, here's the data:"),
+              Text(snapshot.data, style: Theme.of(context).textTheme.display1)
+            ],)
+            :
+            snapshot.hasError ? Text("An error occurred") : CircularProgressIndicator()
+        ),
+      ),
+    );
 }
